ad57e48c0e1935f6fb844a8ef83e2d45617db201
refact0r <yifanw06@gmail.com>
2024-02-25 21:50:28 -0800

    config stuff, remove boilerplate

--- app/src/routes/+page.svelte
+++ app/src/routes/+page.svelte
@@ -1,17 +1,15 @@
 <script lang="ts">
 	import Card from '../components/Card.svelte';
-	import Welcome from '../components/Welcome.svelte';
 	import type { PageData } from './$types';
 
 	export let data: PageData;
 </script>
 
-<section>
-	{#if data.posts.length}
-		{#each data.posts as post}
-			<Card {post} />
-		{/each}
-	{:else}
-		<Welcome />
-	{/if}
-</section>
+<div class="container">
+	<div class="banner"></div>
+</div>
+
+<style lang="scss">
+	.container {
+	}
+</style>
