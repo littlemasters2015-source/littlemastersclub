1611efc72639f462cf52cc80d701115ca62b6c87
refact0r <yifanw06@gmail.com>
2024-03-20 23:08:22 -0700

    basic layout

--- app/src/routes/+layout.svelte
+++ app/src/routes/+layout.svelte
@@ -2,6 +2,9 @@
 	import { isPreviewing, VisualEditing } from '@sanity/visual-editing/svelte';
 	import { page } from '$app/stores';
 	import LiveMode from '../components/LiveMode.svelte';
+	import '../global.css';
+	import '@fontsource-variable/outfit';
+	import logo from '$lib/assets/lmc-logo.jpg';
 </script>
 
 {#if $isPreviewing}
@@ -12,30 +15,23 @@
 {/if}
 
 <div class="container">
-	<header class="header">
-		<a class="header__title" href="/">SvelteKit + Sanity</a>
+	<header>
+		<div class="title">
+			<img class="logo" src={logo} alt="Little Masters Club" />
+			<h1>Little Masters Club | <span>小大师聚乐部</span></h1>
+		</div>
+		<nav>
+			<a href="/">Home</a>
+			<a href="/about">About</a>
+			<a href="/events">Events</a>
+			<a href="/programs">Programs</a>
+			<a href="/programs">Join</a>
+		</nav>
 	</header>
 	<main>
 		<slot />
 	</main>
-	<footer class="footer">
-		<p class="footer__text">
-			Made with <svg
-				data-sanity-icon="heart-filled"
-				width="1em"
-				height="1em"
-				viewBox="0 0 25 25"
-				fill="none"
-				xmlns="http://www.w3.org/2000/svg"
-				><path
-					d="M17 16C15.8 17.3235 12.5 20.5 12.5 20.5C12.5 20.5 9.2 17.3235 8 16C5.2 12.9118 4.5 11.7059 4.5 9.5C4.5 7.29412 6.1 5.5 8.5 5.5C10.5 5.5 11.7 6.82353 12.5 8.14706C13.3 6.82353 14.5 5.5 16.5 5.5C18.9 5.5 20.5 7.29412 20.5 9.5C20.5 11.7059 19.8 12.9118 17 16Z"
-					fill="currentColor"
-					stroke="currentColor"
-					stroke-width="1.2"
-				/></svg
-			> at Sanity
-		</p>
-	</footer>
+	<footer class="footer"></footer>
 </div>
 
 {#if $isPreviewing}
@@ -44,77 +40,44 @@
 {/if}
 
 <style>
-	.container {
-		margin: 0 auto;
-	}
-
-	main {
-		margin-top: 45px;
-	}
-
-	.header {
-		display: flex;
-		padding: 0 var(--space-1);
-		border-bottom: 1px solid #ced2d9;
-
-		z-index: 10;
-		background: var(--white);
-		position: fixed;
-		left: 0;
-		right: 0;
-		top: 0;
-	}
-
-	.header .header__title {
-		font-weight: 800;
-		font-size: var(--font-size-3);
-		line-height: var(--line-height-1);
-		padding-left: var(--space-2);
-		margin: var(--space-3) 0;
-		text-decoration: none;
-		color: var(--black);
-	}
-
-	.footer {
-		display: flex;
-		justify-content: flex-end;
-		padding: 0 var(--space-3);
-	}
-
-	.footer .footer__text {
-		font-size: var(--font-size-1);
-		line-height: var(--line-height-1);
+	header {
+		padding: 1rem 2rem;
 		display: flex;
 		align-items: center;
-		gap: 2px;
+		justify-content: space-between;
+	}
+	.title {
+		display: flex;
+		align-items: center;
+		gap: 1rem;
+	}
+	nav {
+		display: flex;
+		gap: 2rem;
+	}
+	nav a {
+		text-decoration: none;
+		color: var(--txt);
+		font-weight: 500;
+		font-size: 1.2rem;
 	}
 
-	@media (min-width: 575px) {
-		.container {
-			max-width: var(--max-width-1);
-			padding: 0 var(--space-4);
-		}
+	h1 {
+		margin: 0;
+		font-size: 1.6rem;
+	}
+	h1 span {
+		display: inline-block;
+		font-size: 0.8em;
+		margin-top: 0.2rem;
+		vertical-align: text-top;
+	}
 
-		main {
-			margin-top: unset;
-		}
-
-		.header {
-			position: unset;
-			border-bottom: none;
-			margin: var(--space-3) 0;
-			padding: var(--space-2) 0;
-			background: unset;
-		}
-
-		.header .header__title {
-			margin: var(--space-3) 0 var(--space-2);
-			font-size: var(--font-size-5);
-		}
-
-		.footer {
-			margin: var(--space-3) 0;
-		}
+	.logo {
+		height: 3.5rem;
+		width: 3.5rem;
+		border-radius: 1rem;
+		border: var(--border);
 	}
 
 	.preview-toggle {
--- app/src/routes/+page.svelte
+++ app/src/routes/+page.svelte
@@ -1,21 +1,24 @@
 <script lang="ts">
-	import { useQuery } from '@sanity/svelte-loader';
-	import Card from '../components/Card.svelte';
-	import Welcome from '../components/Welcome.svelte';
-	import type { PageData } from './$types';
-
-	export let data: PageData;
-	const q = useQuery(data);
-
-	$: ({ data: posts } = $q);
+	import banner from '$lib/assets/banner.jpg';
 </script>
 
-<section>
-	{#if posts.length}
-		{#each posts as post}
-			<Card {post} />
-		{/each}
-	{:else}
-		<Welcome />
-	{/if}
-</section>
+<div class="page">
+	<section>
+		<img class="banner" src={banner} alt="banner" />
+	</section>
+</div>
+
+<style>
+	.page {
+		padding: 1rem 2rem;
+	}
+
+	.banner {
+		width: 100%;
+		height: 30rem;
+		object-fit: cover;
+		object-position: 50% 70%;
+		border-radius: 2rem;
+		border: var(--border);
+	}
+</style>
