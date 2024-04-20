6ec1363a7448af52267959d5fb7157171fffb855
ehx7 <84419774+ehx7@users.noreply.github.com>
2024-04-19 21:40:24 -0500

    volunteer +page.svelte

---
+++ app/src/routes/volunteer/+page.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+</script>
+
+<div class="page">
+	<h1>Volunteer Awards Q&A and Application Guideline</h1>
+	<p>Welcome to the Little Masters Clubs(LMC), where everyone is a volunteer. Volunteering goes a long way towards creating a healthy community.  You’re setting a great example which delivers a powerful message that encourages others to take action. Volunteers are rewarded as below. LMC thanks you for your contribution and will try our best to recognize your efforts.</p>
+    <p> For details, <a href = "https://docs.google.com/document/d/1HEZU8heumlp-JraGGOsri1QLQPNmomRP-ryI2Aye6j0/edit#heading=h.gjdgxs"> click here</a></p>
+	<p1> Questions? Please contact <a href="mailto:volunteeraward@littlemastersclub.org"> volunteeraward@littlemastersclub.org</a></p1>
+
+
+</div>
+<style lang="scss">
+    h1{
+        text-align: center;
+        padding-bottom: 20px;
+    }
+	.page {
+		margin: auto;
+		max-width: 55rem;
+		padding: 0rem 1.5rem;
+	}
+    
+    
+</style>
