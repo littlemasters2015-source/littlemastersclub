27b7c567750c1d1c5df10128d9538e5922a14769
refact0r <yifanw06@gmail.com>
2024-06-24 15:50:51 -0700

    youtube embed

--- app/src/routes/+page.svelte
+++ app/src/routes/+page.svelte
@@ -38,6 +38,20 @@
 		</div>
 	</section>
 
+	<section class="youtube">
+		<iframe
+			width="1056"
+			height="594"
+			src="https://www.youtube-nocookie.com/embed/AyDXMj-lnzU?si=d3so5hBTkQydBJIT"
+			title="YouTube video player"
+			frameborder="0"
+			allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+			referrerpolicy="strict-origin-when-cross-origin"
+			allowfullscreen
+			credentialless
+		/>
+	</section>
+
 	<section class="support">
 		<img src={support} alt="support" />
 		<div class="box">
@@ -210,6 +224,13 @@
 			.description {
 				@include line-clamp(2);
 			}
+		}
+	}
+	.youtube {
+		@include border;
+		overflow: hidden;
+		iframe {
+			display: block;
 		}
 	}
 </style>
