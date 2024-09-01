db8ad7e702141e48ff7dac52ec66aef0db49c0b5
refact0r <34758569+refact0r@users.noreply.github.com>
2024-09-01 01:17:07 -0700

    order studio correctly

--- studio/structure.ts
+++ studio/structure.ts
@@ -7,9 +7,18 @@ export const structure: StructureResolver = (S) =>
       S.listItem()
         .title('Home Page')
         .child(S.document().schemaType('homePage').documentId('homePage')),
-      ...S.documentTypeListItems().filter(
-        (item) => !['program', 'homePage'].includes(item.getId() ?? ''),
-      ),
+      ...S.documentTypeListItems()
+        .filter((item) => !['program', 'homePage'].includes(item.getId() ?? ''))
+        .toSorted((a, b) => {
+          // always put pages at the front, sort alphabetically otherwise
+          if (a.getId() === 'page' && b.getId() !== 'page') {
+            return -1
+          } else if (b.getId() === 'page' && a.getId() !== 'page') {
+            return 1
+          } else {
+            return (a.getId() ?? '').localeCompare(b.getId() ?? '')
+          }
+        }),
       S.listItem()
         .title('Current Programs')
         .child(
