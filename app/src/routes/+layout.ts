0a98b45a3e571947530261a6a579dc1addaa67cc
refact0r <34758569+refact0r@users.noreply.github.com>
2024-11-09 23:42:48 -0800

    test posthog

--- app/src/routes/+layout.ts
+++ app/src/routes/+layout.ts
@@ -1,5 +1,7 @@
 import { setPreviewing } from '@sanity/visual-editing/svelte';
 import type { LayoutLoad } from './$types';
+import posthog from 'posthog-js';
+import { browser } from '$app/environment';
 
 export const load: LayoutLoad = ({ data }) => {
 	// The `event.data.preview` value received here is exposed by the
@@ -8,6 +10,13 @@ export const load: LayoutLoad = ({ data }) => {
 	// `@sanity/visual-editing/svelte` exports two helpers for setting and getting
 	// preview state on the client: `setPreviewing` and `isPreviewing`.
 	setPreviewing(preview);
+
+	if (browser) {
+		posthog.init('phc_casjJqwZZBcMdLSlSPxXi6M6MBCqIr9C5zGib0n2i8Y', {
+			api_host: 'https://littlemastersclub.vercel.app/ingest',
+			ui_host: 'https://us.posthog.com'
+		});
+	}
 
 	return {
 		...data
