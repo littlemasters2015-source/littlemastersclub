e40fe4e081673e34c27b7fab0bf78743672f3595
refact0r <34758569+refact0r@users.noreply.github.com>
2025-01-25 20:31:52 -0800

    remove posthog

--- app/src/routes/+layout.ts
+++ app/src/routes/+layout.ts
@@ -1,7 +1,5 @@
 import { setPreviewing } from '@sanity/visual-editing/svelte';
 import type { LayoutLoad } from './$types';
-import posthog from 'posthog-js';
-import { browser } from '$app/environment';
 
 export const load: LayoutLoad = ({ data }) => {
 	// The `event.data.preview` value received here is exposed by the
@@ -10,15 +8,6 @@ export const load: LayoutLoad = ({ data }) => {
 	// `@sanity/visual-editing/svelte` exports two helpers for setting and getting
 	// preview state on the client: `setPreviewing` and `isPreviewing`.
 	setPreviewing(preview);
-
-	if (browser) {
-		console.log();
-		posthog.init('phc_casjJqwZZBcMdLSlSPxXi6M6MBCqIr9C5zGib0n2i8Y', {
-			api_host: 'https://littlemastersclub.vercel.app/ingest',
-			ui_host: 'https://us.posthog.com',
-			person_profiles: 'identified_only'
-		});
-	}
 
 	return {
 		...data
