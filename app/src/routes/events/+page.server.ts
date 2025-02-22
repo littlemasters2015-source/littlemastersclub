906e865d2dec54395495da0c8f404aa75f23a653
refact0r <34758569+refact0r@users.noreply.github.com>
2025-02-21 21:47:13 -0800

    log

--- app/src/routes/events/+page.server.ts
+++ app/src/routes/events/+page.server.ts
@@ -4,6 +4,7 @@ import type { PageServerLoad } from './$types';
 export const load: PageServerLoad = async (event) => {
 	const { loadQuery } = event.locals;
 	const initial = await loadQuery<Event[]>(query);
+	console.log(initial);
 
 	return {
 		query,
