56d385c8b485ddf9f6bf5161b3726d59db1668b7
refact0r <34758569+refact0r@users.noreply.github.com>
2025-02-23 21:36:43 -0800

    make it faster

--- app/src/routes/api/revalidate/+server.ts
+++ app/src/routes/api/revalidate/+server.ts
@@ -6,7 +6,7 @@ type ResponseType = any;
 
 async function revalidateSlug(slug: string): Promise<boolean> {
 	// Set a delay to allow Sanity data to correctly propagate.
-	await new Promise((resolve) => setTimeout(resolve, 8000));
+	await new Promise((resolve) => setTimeout(resolve, 4000));
 	console.log(`Revalidating: ${env.SITE_URL}/${slug}`);
 	const res = await fetch(`${env.SITE_URL}/${slug}`, {
 		cache: 'no-store',
